95129b4185cd8b74e0755dca3123c1ddb53031f2
kareem mahfouz <samkrym123@gmail.com>
2024-10-12 01:55:46 +0300

    splash view done

--- Bloc and MVVM/bookly_app/lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ Bloc and MVVM/bookly_app/lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,68 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SvgPicture.asset(
+          MyAssets.logo,
+          height: 50,
+        ),
+        SizedBox(height: MediaQuery.sizeOf(context).height * .03),
+        SlideTransition(
+          position: slidingAnimation,
+          child: const Text(
+            'Books Made Easy',
+            textAlign: TextAlign.center,
+          ),
+        )
+      ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+
+    final CurvedAnimation curvedAnimation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.easeOutCubic,
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 3), end: const Offset(0, 0))
+            .animate(curvedAnimation);
+
+    animationController.forward();
   }
 }
