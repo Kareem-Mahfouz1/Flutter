094b4100cd7515240972373251ec57649cce2044
kareem mahfouz <samkrym123@gmail.com>
2024-10-22 19:03:50 +0300

    cached network image

--- Bloc and MVVM/bookly_app/lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ Bloc and MVVM/bookly_app/lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,4 +1,6 @@
-import '../../../../../core/utils/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'package:flutter/material.dart';
 
 class CustomBookItem extends StatelessWidget {
@@ -9,13 +11,15 @@ class CustomBookItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 0.65,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(imageUrl),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: CachedNetworkImage(
+          errorWidget: (context, url, error) => const Icon(
+            FontAwesomeIcons.triangleExclamation,
+            size: 35,
           ),
+          imageUrl: imageUrl,
+          fit: BoxFit.cover,
         ),
       ),
     );
